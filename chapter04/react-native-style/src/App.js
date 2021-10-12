66ec650dc470ceadca6ef2a32c03cf7e3618d868
rlathal009 <rlathal009@gmail.com>
2021-10-13 01:12:40 +0900

    style

--- chapter04/react-native-style/src/App.js
+++ chapter04/react-native-style/src/App.js
@@ -1,36 +1,13 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { viewStyles, textStyles } from './styles';
+import { Header, Contents, Footer } from './components/Layout';
+import ShadowBox from './components/ShadowBox';
 
 const App = () => {
     return(
-        <View
-        style={{
-            flex: 1,
-            backgroundColor: '#fff',
-            alignItems: 'center',
-            justifyContent: 'center',
-        }}
-        >
-            <Text
-            style={{
-                padding:10,
-                fontSize: 26,
-                fontWeight: '600',
-                color: 'black',
-            }}
-            >
-                Inline Styling - Text
-            </Text>
-            <Text
-            style = {{
-                padding:10,
-                fontSize: 26,
-                fontWeight: '400',
-                color: 'red',
-            }}
-            >
-                Inline Styling - Error
-            </Text>
+        <View style ={viewStyles.container}>
+           <ShadowBox />
         </View>
     );
 };
