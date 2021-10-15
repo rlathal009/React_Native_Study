b27907f63217dd4c75a92e09c901cbc2f9cc864f
rlathal009 <rlathal009@gmail.com>
2021-10-16 00:47:40 +0900

    style

--- chapter04/react-native-style/src/App.js
+++ chapter04/react-native-style/src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Switch } from 'react-native';
 import { viewStyles, textStyles } from './styles';
 import { Header, Contents, Footer } from './components/Layout';
 import ShadowBox from './components/ShadowBox';
-import styled from 'styled-components/native'
+import styled, { ThemeProvider } from 'styled-components/native'
 import Button from './components/Button';
 import Input from './components/Input';
+import { lightTheme, darkTheme } from './theme';
+
 
 const Container = styled.View`
     flex: 1;
-    background-color: #ffffff;
+    background-color: ${props => props.theme.background};
     align-items: center;
     justify-content : center;
 `;
 
 const App = () => {
+    const [isDark, setIsDark] = useState(false);
+    const _toggleSwitch = () => setIsDark(!isDark);
+
     return(
-        <Container>
-            <Button title="Hanbit" />
-            <Button title="React Native" />
-            <Input />
-        </Container>
+        <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+            <Container>
+                <Switch value={isDark} onValueChange={_toggleSwitch} />
+                <Button title="Hanbit" />
+                <Button title="React Native" />
+                <Input borderColor="#3498db" />
+                <Input borderColor="#9b59b6" />
+            </Container>
+        </ThemeProvider>
     );
 };
 
