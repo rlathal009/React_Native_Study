2a402316142844f981219d31545985fec4c9cd52
rlathal009 <rlathal009@gmail.com>
2021-10-15 23:55:24 +0900

    style

--- chapter04/react-native-style/src/App.js
+++ chapter04/react-native-style/src/App.js
@@ -3,12 +3,24 @@ import { View, Text } from 'react-native';
 import { viewStyles, textStyles } from './styles';
 import { Header, Contents, Footer } from './components/Layout';
 import ShadowBox from './components/ShadowBox';
+import styled from 'styled-components/native'
+import Button from './components/Button';
+import Input from './components/Input';
+
+const Container = styled.View`
+    flex: 1;
+    background-color: #ffffff;
+    align-items: center;
+    justify-content : center;
+`;
 
 const App = () => {
     return(
-        <View style ={viewStyles.container}>
-           <ShadowBox />
-        </View>
+        <Container>
+            <Button title="Hanbit" />
+            <Button title="React Native" />
+            <Input />
+        </Container>
     );
 };
 
