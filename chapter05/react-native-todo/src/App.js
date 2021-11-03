529646851a5ab43c0503eb5140ffdc73502017af
rlathal009 <rlathal009@gmail.com>
2021-11-03 21:19:36 +0900

    chapter5

---
+++ chapter05/react-native-todo/src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { StatusBar } from 'react-native'; 
+import styled, { ThemeProvider } from 'styled-components/native';
+import { theme } from './theme';
+import Input from './components/Input';
+
+
+const Container= styled.SafeAreaView`
+    flex: 1;
+    background-color: ${({ theme }) => theme.background};
+    align-items: center;
+    justify-content: flex-start;
+`;
+
+const Title = styled.Text`
+    font-size: 40px;
+    font-weight: 600;
+    color: ${({ theme }) => theme.main};
+    align-self: flex-start;
+    margin: 20px;
+`;
+
+export default function App(){
+    return(
+        <ThemeProvider theme={theme}>
+            <Container>
+                <StatusBar
+                    barStyle="light-content"
+                    backgroundColor={theme.background}
+                />
+                <Title>TODO List</Title>
+                <Input />
+            </Container>
+        </ThemeProvider>
+    );
+};
