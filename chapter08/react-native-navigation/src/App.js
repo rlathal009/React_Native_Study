90d3661a30b08dd6091369fbac31d693bf3cdb3b
rlathal009 <rlathal009@gmail.com>
2021-11-21 02:58:06 +0900

    navigation

--- chapter08/react-native-navigation/src/App.js
+++ chapter08/react-native-navigation/src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import styled from "styled-components/native";
 import { NavigationContainer } from "@react-navigation/native";
 import StackNavigation from "./navigations/Stack";
+import TabNavigation from "./navigations/Tab";
 
 const App = () => {
     return (
         <NavigationContainer>
-            <StackNavigation/>
+            <TabNavigation />
         </NavigationContainer>
     );
 };
