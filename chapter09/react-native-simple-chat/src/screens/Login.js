dee09db13d6d1ad5cff9f2ed1bcc6109bfb54f4c
Somi Kim <rlathal009@gmail.com>
2021-12-25 18:02:28 -0800

    chat

---
+++ chapter09/react-native-simple-chat/src/screens/Login.js
@@ -0,0 +1,49 @@
+import React, { useState, useRef } from "react";
+import styled from 'styled-components/native';
+import { Text, Button } from 'react-native';
+import { Image , Input } from "../components";
+import { images } from "../utils/images";
+import { TouchableWithoutFeedback, Keyboard } from "react-native";
+
+const Container = styled.View`
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+    background-color: ${({theme}) => theme.background};
+    padding: 20px;
+`;
+
+const Login = ({ navigation }) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const passwordRef = useRef();
+
+    return(
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <Container>
+            <Image url={images.logo} imageStyle={{ borderRadius: 8 }} />
+            <Input
+                label="Email"
+                value={email}
+                onChangeText={text => setEmail(text)}
+                onSubmitEditing={() => passwordRef.current.focus()}
+                placeholder="Email"
+                returnKeyType="next"
+            />
+            <Input
+                ref={passwordRef}
+                label="Password"
+                value={password}
+                onChangeText={text => setPassword(text)}
+                onSubmitEditing={() => {}}
+                placeholder="Password"
+                returnKeyType="done"
+                isPassword
+            />
+            <Button title="Signup" onPress={() => navigation.navigate('Signup')} />
+        </Container>
+        </TouchableWithoutFeedback>
+    );
+};
+
+export default Login;
