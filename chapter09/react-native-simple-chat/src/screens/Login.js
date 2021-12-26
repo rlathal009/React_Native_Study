cd617c2586b098bde66d733f53cf2aaecae04ba1
Somi Kim <rlathal009@gmail.com>
2021-12-26 01:21:05 -0800

    chat

--- chapter09/react-native-simple-chat/src/screens/Login.js
+++ chapter09/react-native-simple-chat/src/screens/Login.js
@@ -1,31 +1,62 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import styled from 'styled-components/native';
-import { Text, Button } from 'react-native';
-import { Image , Input } from "../components";
+import { Text } from 'react-native';
+import { Image , Input, Button } from "../components";
 import { images } from "../utils/images";
-import { TouchableWithoutFeedback, Keyboard } from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { validateEmail, removeWhitespace } from "../utils/common";
 
 const Container = styled.View`
     flex: 1;
     justify-content: center;
     align-items: center;
     background-color: ${({theme}) => theme.background};
-    padding: 20px;
+    padding: 0 20px;
+`;
+
+const ErrorText = styled.Text`
+    align-items: flex-start;
+    width: 100%;
+    height: 20px;
+    margin-bottom: 10px;
+    line-height: 20px;
+    color: ${({ theme }) => theme.errorText};
 `;
 
 const Login = ({ navigation }) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const passwordRef = useRef();
+    const [errorMessage, setErrorMessage] = useState('');
+    const [disabled, setDisabled] = useState(true);
+
+    useEffect(() => {
+        setDisabled(!(email && password && !errorMessage));
+    }, [email, password, errorMessage]);
+
+    const _handleEmailChange = email => {
+        const changedEmail = removeWhitespace(email);
+        setEmail(changedEmail);
+        setErrorMessage(
+            validateEmail(changedEmail) ? '' : 'Please verify your email.'
+        );
+    };
+    const _handlePasswordChange = password => {
+        setPassword(removeWhitespace(password));
+    };
+    const _handleLoginButtonPress = () => {};
 
     return(
-        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-            <Container>
+        <KeyboardAwareScrollView
+            contentContainerStyle={{ flex: 1 }}
+            extraScrollHeight={20}
+        >
+        <Container>
             <Image url={images.logo} imageStyle={{ borderRadius: 8 }} />
             <Input
                 label="Email"
                 value={email}
-                onChangeText={text => setEmail(text)}
+                onChangeText={_handleEmailChange}
                 onSubmitEditing={() => passwordRef.current.focus()}
                 placeholder="Email"
                 returnKeyType="next"
@@ -34,15 +65,25 @@ const Login = ({ navigation }) => {
                 ref={passwordRef}
                 label="Password"
                 value={password}
-                onChangeText={text => setPassword(text)}
-                onSubmitEditing={() => {}}
+                onChangeText={_handlePasswordChange}
+                onSubmitEditing={_handleLoginButtonPress}
                 placeholder="Password"
                 returnKeyType="done"
                 isPassword
             />
-            <Button title="Signup" onPress={() => navigation.navigate('Signup')} />
+            <ErrorText>{errorMessage}</ErrorText>
+            <Button 
+                title="Login"
+                onPress={_handleLoginButtonPress}
+                disabled={disabled}
+            />
+            <Button
+                title="Sign up with email" 
+                onPress={() => navigation.navigate('Signup')}
+                isFilled={false}
+            />
         </Container>
-        </TouchableWithoutFeedback>
+        </KeyboardAwareScrollView>
     );
 };
 
